687d22bceaa6483886775a2379ce1f12da1ff9fd
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-25 17:01:30 -0500

    Prosemirror PoC

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -7,52 +7,92 @@
  * https://github.com/github/markdown-toolbar-element/blob/master/LICENSE
  */
 
-import { extend } from 'flarum/extend';
-import TextEditor from 'flarum/components/TextEditor';
-import MarkdownArea from 'mdarea';
+import { extend, override } from 'flarum/extend';
+import ProseMirrorEditor from 'flarum/editor/ProseMirrorEditor';
+import {
+  schema, defaultMarkdownParser,
+  defaultMarkdownSerializer
+} from "prosemirror-markdown";
+import { keymap } from 'prosemirror-keymap';
+import { buildInputRules } from './util/inputrules';
+import { buildKeymap } from './util/buildKeymap';
 
-import MarkdownToolbar from './components/MarkdownToolbar';
-import MarkdownButton from './components/MarkdownButton';
+/**
+ * For some reason, our webpack config doesn't recognize the OrderedMap utility
+ * used by prosemirror markdown.
+ */
+function fixOrderedMap(map) {
+  const result = {};
 
-app.initializers.add('flarum-markdown', function(app) {
-  let index = 1;
+  for (let i = 0; i < map.size; i++) {
+    result[map.content[2 * i]] = map.content[2 * i + 1];
+  }
 
-  extend(TextEditor.prototype, 'oninit', function() {
-    this.textareaId = 'textarea'+(index++);
+  return result;
+}
+
+app.initializers.add('flarum-markdown', function (app) {
+  override(ProseMirrorEditor.prototype, 'parseInitialValue', function (original, val) {
+    return defaultMarkdownParser.parse(val);
   });
 
-  extend(TextEditor.prototype, 'view', function(vdom) {
-    vdom.children[0].attrs.id = this.textareaId;
+  override(ProseMirrorEditor.prototype, 'serializeContent', function (original, doc) {
+    return defaultMarkdownSerializer.serialize(doc);
   });
 
-  extend(TextEditor.prototype, 'oncreate', function() {
-    this.editor = new MarkdownArea(this.$('textarea')[0], {
-      keyMap: {
-        indent: ['Ctrl+m'],
-        outdent: ['Ctrl+M']
-      }
-    });
+  override(ProseMirrorEditor.prototype, 'buildSchemaConfig', function (original, doc) {
+    const newSpec = {
+      nodes: fixOrderedMap(schema.spec.nodes),
+      marks: fixOrderedMap(schema.spec.marks)
+    };
+
+    return newSpec;
   });
 
-  extend(TextEditor.prototype, 'onremove', function () {
-    this.editor.destroy();
+  extend(ProseMirrorEditor.prototype, 'buildPluginItems', function (items) {
+    items.add('markdownInputrules', buildInputRules(this.schema));
+    items.add('markdownKeybinds', keymap(buildKeymap(this.schema)));
   });
 
-  extend(TextEditor.prototype, 'toolbarItems', function(items) {
-    const tooltip = name => app.translator.trans(`flarum-markdown.forum.composer.${name}_tooltip`);
 
-    items.add('markdown', (
-      <MarkdownToolbar for={this.textareaId}>
-        <MarkdownButton title={tooltip('header')} icon="fas fa-heading" style={{ prefix: '### ' }} />
-        <MarkdownButton title={tooltip('bold')} icon="fas fa-bold" style={{ prefix: '**', suffix: '**', trimFirst: true }} hotkey="b" />
-        <MarkdownButton title={tooltip('italic')} icon="fas fa-italic" style={{ prefix: '_', suffix: '_', trimFirst: true }} hotkey="i" />
-        <MarkdownButton title={tooltip('quote')} icon="fas fa-quote-left" style={{ prefix: '> ', multiline: true, surroundWithNewlines: true }} />
-        <MarkdownButton title={tooltip('code')} icon="fas fa-code" style={{ prefix: '`', suffix: '`', blockPrefix: '```', blockSuffix: '```' }} />
-        <MarkdownButton title={tooltip('link')} icon="fas fa-link" style={{ prefix: '[', suffix: '](https://)', replaceNext: 'https://', scanFor: 'https?://' }} />
-        <MarkdownButton title={tooltip('image')} icon="fas fa-image" style={{ prefix: '![', suffix: '](https://)', replaceNext: 'https://', scanFor: 'https?://' }} />
-        <MarkdownButton title={tooltip('unordered_list')} icon="fas fa-list-ul" style={{ prefix: '- ', multiline: true, surroundWithNewlines: true }} />
-        <MarkdownButton title={tooltip('ordered_list')} icon="fas fa-list-ol" style={{ prefix: '1. ', multiline: true, orderedList: true }} />
-      </MarkdownToolbar>
-    ), 100);
-  });
+  // let index = 1;
+
+  // extend(TextEditor.prototype, 'oninit', function() {
+  //   this.textareaId = 'textarea'+(index++);
+  // });
+
+  // extend(TextEditor.prototype, 'view', function(vdom) {
+  //   vdom.children[0].attrs.id = this.textareaId;
+  // });
+
+  // extend(TextEditor.prototype, 'oncreate', function() {
+  //   this.editor = new MarkdownArea(this.$('textarea')[0], {
+  //     keyMap: {
+  //       indent: ['Ctrl+m'],
+  //       outdent: ['Ctrl+M']
+  //     }
+  //   });
+  // });
+
+  // extend(TextEditor.prototype, 'onremove', function () {
+  //   this.editor.destroy();
+  // });
+
+  // extend(TextEditor.prototype, 'toolbarItems', function(items) {
+  //   const tooltip = name => app.translator.trans(`flarum-markdown.forum.composer.${name}_tooltip`);
+
+  //   items.add('markdown', (
+  //     <MarkdownToolbar for={this.textareaId}>
+  //       <MarkdownButton title={tooltip('header')} icon="fas fa-heading" style={{ prefix: '### ' }} />
+  //       <MarkdownButton title={tooltip('bold')} icon="fas fa-bold" style={{ prefix: '**', suffix: '**', trimFirst: true }} hotkey="b" />
+  //       <MarkdownButton title={tooltip('italic')} icon="fas fa-italic" style={{ prefix: '_', suffix: '_', trimFirst: true }} hotkey="i" />
+  //       <MarkdownButton title={tooltip('quote')} icon="fas fa-quote-left" style={{ prefix: '> ', multiline: true, surroundWithNewlines: true }} />
+  //       <MarkdownButton title={tooltip('code')} icon="fas fa-code" style={{ prefix: '`', suffix: '`', blockPrefix: '```', blockSuffix: '```' }} />
+  //       <MarkdownButton title={tooltip('link')} icon="fas fa-link" style={{ prefix: '[', suffix: '](https://)', replaceNext: 'https://', scanFor: 'https?://' }} />
+  //       <MarkdownButton title={tooltip('image')} icon="fas fa-image" style={{ prefix: '![', suffix: '](https://)', replaceNext: 'https://', scanFor: 'https?://' }} />
+  //       <MarkdownButton title={tooltip('unordered_list')} icon="fas fa-list-ul" style={{ prefix: '- ', multiline: true, surroundWithNewlines: true }} />
+  //       <MarkdownButton title={tooltip('ordered_list')} icon="fas fa-list-ol" style={{ prefix: '1. ', multiline: true, orderedList: true }} />
+  //     </MarkdownToolbar>
+  //   ), 100);
+  // });
 });
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,3 +1,13 @@
 const config = require('flarum-webpack-config');
 
-module.exports = config();
+module.exports = (function () {
+  const _exports = config();
+
+  _exports.externals[0]['prosemirror-commands'] = 'prosemirror.commands';
+  _exports.externals[0]['prosemirror-model'] = 'prosemirror.model';
+  _exports.externals[0]['prosemirror-keymap'] = 'prosemirror.keymap';
+  _exports.externals[0]['prosemirror-state'] = 'prosemirror.state';
+  _exports.externals[0]['prosemirror-transform'] = 'prosemirror.transform';
+
+  return _exports;
+})();
