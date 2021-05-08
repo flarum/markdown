6d33a48ccfde441e810638c72ed216ede4fc0815
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-05-07 21:55:51 -0400

    Move style to css

--- js/src/forum/components/MarkdownToolbar.js
+++ js/src/forum/components/MarkdownToolbar.js
@@ -2,7 +2,7 @@ import Component from 'flarum/Component';
 
 export default class MarkdownToolbar extends Component {
   view(vnode) {
-    return <div id="MarkdownToolbar" style={{ display: 'inline-block' }}>
+    return <div class="MarkdownToolbar">
       {vnode.children}
     </div>;
   }
